02b8e65e8256deb4dae70d78d7cb5fa889fcc57f
Bryce McLean <mclean.bryce@gmail.com>
2015-04-09 21:10:43 -0600

    Fixing secret_token and seeds. Adding gemset and ruby version.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => ‘rskill@windreturns.com’, :password => ‘goboys123’, :password_confirmation => ’goboys123’)
+AdminUser.create!(:email => 'rskill@windreturns.com', :password => 'goboys123', :password_confirmation => 'goboys123')
